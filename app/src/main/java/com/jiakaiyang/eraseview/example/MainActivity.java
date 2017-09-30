57ea900b5c7099c49f56b1aa4c82ba580d79a19a
jiakaiyang <kaiyangjia@163.com>
2017-09-30 10:23:59 +0800

    add library

--- app/src/main/java/com/jiakaiyang/eraseview/example/MainActivity.java
+++ app/src/main/java/com/jiakaiyang/eraseview/example/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jiakaiyang.eraseview;
+package com.jiakaiyang.eraseview.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ eraseview-lib/src/main/java/com/jiakaiyang/eraseview/lib/EraseImageView.java
@@ -0,0 +1,25 @@
+package com.jiakaiyang.eraseview.lib;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+/**
+ * Created by jia on 2017/9/30.
+ * <p>
+ * The EraseImageView
+ */
+
+public class EraseImageView extends AppCompatImageView {
+    public EraseImageView(Context context) {
+        super(context);
+    }
+
+    public EraseImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EraseImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
