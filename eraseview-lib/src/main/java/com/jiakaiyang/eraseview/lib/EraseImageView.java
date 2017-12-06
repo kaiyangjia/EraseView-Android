8bb555fd55d5732db2edbe25acdac0e9279e45c3
jiakaiyang <kaiyangjia@163.com>
2017-12-06 15:01:05 +0800

    test something

--- eraseview-lib/src/main/java/com/jiakaiyang/eraseview/lib/EraseImageView.java
+++ eraseview-lib/src/main/java/com/jiakaiyang/eraseview/lib/EraseImageView.java
@@ -1,13 +1,18 @@
 package com.jiakaiyang.eraseview.lib;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
+import android.graphics.Region;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Created by jia on 2017/9/30.
@@ -16,7 +21,13 @@ import android.util.AttributeSet;
  */
 
 public class EraseImageView extends AppCompatImageView {
+    private static final String TAG = "EraseImageView";
+
     private Paint erasePaint;
+
+    // The layer eraseToLayer will not be erase
+    // Between top and eraseToLayer will be erase
+    private int eraseToLayer = Layer.PARENT_BACKGROUND;
 
     public EraseImageView(Context context) {
         super(context);
@@ -43,16 +54,68 @@ public class EraseImageView extends AppCompatImageView {
 
 
         Rect rect = new Rect(0, 0, getWidth() / 2, getHeight() / 2);
-        canvas.drawRect(rect, erasePaint);
-//        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+//        boolean clipResult = canvas.clipRect(rect);
+//        Log.i(TAG, "onDraw: clipResult: " + clipResult);
+        //        canvas.drawRect(rect, recerasePaint);
+
+        Rect roundRect = new Rect();
+        getRoundRect(roundRect);
+
+        Drawable background = getTargetDrawable();
+        background.setBounds(roundRect);
+        background.draw(canvas);
     }
 
     private Paint createErasePaint() {
         Paint paint = new Paint();
         paint.setAntiAlias(true);
-        paint.setColor(0);
+//        paint.setColor(Color.TRANSPARENT);
+        paint.setAlpha(0xFF);
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
 
         return paint;
+    }
+
+    /**
+     * 获取需要被擦出区域的Drawable
+     *
+     * @return
+     */
+    protected Drawable getTargetDrawable() {
+        switch (eraseToLayer) {
+            case Layer.PARENT_BACKGROUND:
+                return ViewUtils.forkBackground((View) getParent());
+            case Layer.CURRENT_BACKGROUND:
+                return ViewUtils.forkBackground(this);
+        }
+
+        return null;
+    }
+
+
+    protected Bitmap getTargetBitmap() {
+        switch (eraseToLayer) {
+            case Layer.PARENT_BACKGROUND:
+                return ViewUtils.getBitmapFromView((View) getParent());
+            case Layer.CURRENT_BACKGROUND:
+                return ViewUtils.getBitmapFromView(this);
+        }
+
+        return null;
+    }
+
+
+    protected void getRoundRect(Rect rect) {
+        switch (eraseToLayer) {
+            case Layer.PARENT_BACKGROUND: {
+                View view = (View) getParent();
+                view.getGlobalVisibleRect(rect);
+                break;
+            }
+            case Layer.CURRENT_BACKGROUND: {
+                getGlobalVisibleRect(rect);
+                break;
+            }
+        }
     }
 }
--- eraseview-lib/src/main/java/com/jiakaiyang/eraseview/lib/ViewUtils.java
+++ eraseview-lib/src/main/java/com/jiakaiyang/eraseview/lib/ViewUtils.java
@@ -1,8 +1,38 @@
 package com.jiakaiyang.eraseview.lib;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+
 /**
  * Created by jia on 2017/12/4.
  */
 
 public class ViewUtils {
+
+    public static Drawable forkBackground(View view) {
+        Drawable drawable = view.getBackground();
+        return drawable.getConstantState().newDrawable();
+    }
+
+
+    public static Drawable forkSrc(ImageView imageView) {
+        return imageView.getDrawable();
+    }
+
+
+    /**
+     * get The View Bitmap, The View must be displayed
+     * @param v
+     * @return
+     */
+    public static Bitmap getBitmapFromView(View v) {
+        Bitmap b = Bitmap.createBitmap( v.getLayoutParams().width, v.getLayoutParams().height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        v.layout(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+        v.draw(c);
+        return b;
+    }
 }
